fdfd9af4596e126ff20a87756c4af43877d8d9ca
Andrey Lukin <luk.andrey@gmail.com>
2018-10-19 23:34:50 -0400

    Hashing of specific files

---
+++ fileEncryption/encrypt.js
@@ -0,0 +1,60 @@
+const crypto = require('crypto'),
+    algorithm = 'aes-256-ctr'
+
+function encrypt(text, password){
+  var cipher = crypto.createCipher(algorithm,password)
+  var crypted = cipher.update(text,'utf8','hex')
+  crypted += cipher.final('hex');
+  return crypted;
+}
+ 
+function decrypt(text, password){
+  var decipher = crypto.createDecipher(algorithm,password)
+  var dec = decipher.update(text,'hex','utf8')
+  dec += decipher.final('utf8');
+  return dec;
+}
+
+function encryptPath(path, password) {
+  let returnString = '';
+  let directories = path.split("/");
+  for(let i = 0; i< directories.length; i+=1) {
+    if(directories[i] == ".") {
+      returnString += ".";
+    } else {
+      returnString += encrypt(directories[i], password);
+    }
+    returnString += "/"
+    if(i == directories.length - 1 && path.charAt(path.length - 1) != '/') {
+      returnString = returnString.slice(0, -1);
+    }
+  }
+  return returnString;
+}
+
+
+function decryptPath(path, password) {
+  let returnString = '';
+  let directories = path.split("/");
+  for(let i = 0; i< directories.length; i+=1) {
+    if(directories[i] == ".") {
+      returnString += ".";
+    } else {
+      returnString += decrypt(directories[i], password);
+    }
+    if(directories[i].length != 0) {
+      returnString += "/"
+    }
+    if(i == directories.length - 1 && path.charAt(path.length - 1) != '/') {
+      returnString = returnString.slice(0, -1);
+    }
+  }
+  return returnString;
+
+}
+
+
+module.exports.encrypt = encrypt;
+module.exports.decrypt = decrypt;
+module.exports.encryptPath = encryptPath;
+module.exports.decryptPath = decryptPath;
---
+++ fileEncryption/fileController.js
@@ -0,0 +1,64 @@
+
+
+var key = 'reejfiunfowifnwopnjihbvhnjkjhghjkjhghjuygbnjuygbnjuygtvbnjuytgfvbnjuytgvbnjuygmopmp';
+ 
+const fs = require('fs');
+const readline = require('readline');
+const { encrypt, decrypt, encryptPath, decryptPath } = require('./encrypt');
+var mkdirp = require('mkdirp');
+
+
+function encryptFile(fileToEncrypt) {
+    let encryptedFile = encryptPath(fileToEncrypt, key);
+    createDirs(encryptedFile);
+    const readFile = readline.createInterface({
+        input: fs.createReadStream(fileToEncrypt),
+        output: fs.createWriteStream(encryptedFile),
+        terminal: false
+      });
+    readFile
+      .on('line', function(line) {
+          this.output.write(`${encrypt(line, key)}\n`);
+    })
+      .on('close', function() {
+        console.log(`Created "${this.output.path}"`);
+    });
+}
+
+function decryptFile(encryptedFile) {
+    let fileToEncrypt = decryptPath(encryptedFile, key);
+    createDirs(fileToEncrypt);
+    const readFile = readline.createInterface({
+        input: fs.createReadStream(encryptedFile),
+        output: fs.createWriteStream(fileToEncrypt),
+        terminal: false
+      });
+    readFile
+      .on('line', function(line) {
+          this.output.write(`${decrypt(line, key)}\n`);
+    })
+      .on('close', function() {
+        console.log(`Created "${this.output.path}"`);
+    });
+}
+
+
+function createDirs(path) {
+    let dirs = path.split("/");
+    if(path.charAt(path.length - 1) == '/') {
+        mkdirp.sync(dirs.splice(0, dirs.length - 2).join("/"));
+    } else {
+        mkdirp.sync(path.split("/").splice(0, dirs.length - 1).join("/"));
+    }
+}
+
+
+
+
+module.exports.encryptFile = encryptFile;
+module.exports.decryptFile = decryptFile;
+
+
+
+
+
