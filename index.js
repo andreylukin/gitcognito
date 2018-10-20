a9eb271815dc3e69b939a0097d0ab49b44fda7bd
Jake Welde <jwelde@seas.upenn.edu>
2018-10-19 22:33:59 -0400

    basic git commands running from gcn

--- index.js
+++ index.js
@@ -1,56 +1,42 @@
+var parse = require('./src/commands.js');
 
 module.exports = () => {
+
   var args = require('minimist')(process.argv.slice(2));
-  // console.dir(args);
+
   command = args._[0];
 
   var fs = require("fs");
 
   if(command === 'init') {
-    // start the gitcognito repo
 
-    var fs = require('fs');
-    if (fs.existsSync(".gcn")) {
-        console.log("reinit");
-        return;
+    parse.init(args);
+    return;
+
+  } else{
+
+    if (!fs.existsSync(".gcn")) {
+      console.log("Error: not a gcn repo");
+      return;
     }
 
-    console.log('Do the init');
+    var contents = fs.readFileSync(".gcn");
+    try {
+      var jsonContent = JSON.parse(contents);
+    } catch(error) {
+      console.log(error);
+    }
 
-    var password;
-    if(args.p) {
-      password = args.p;
+    var password = jsonContent.password;
+
+    // this is a gcn repo and it is initialized. parse commands
+
+    if(command === 'clone') {
+      parse.clone(args,password);
     } else {
-      var generator = require('generate-password');
-      password = generator.generate({
-          length: 16,
-          numbers: true
-      });
+      parse.other(args,password)
     }
 
-    let data = JSON.stringify({
-      password : password
-    });
-    fs.writeFileSync('.gcn', data);
-
-    // 'uEyMTw32v9'
-    console.log(password);
-
-
-    return;
   }
-
-  if (!fs.existsSync(".gcn")) {
-    console.log("Error: not a gcn repo");
-    return;
-  }
-
-  var contents = fs.readFileSync(".gcn");
-  try {
-    var jsonContent = JSON.parse(contents);
-  } catch(error) {
-    console.log(error);
-  }
-  console.log("Encryption password:", jsonContent.password,"\n");
 
 }
